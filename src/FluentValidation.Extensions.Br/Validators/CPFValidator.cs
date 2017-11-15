a892064236aecdb62d9ddd8848bb7940bf90f823
Lucas Loureiro <lucas.loureiro@esx.com.br>
2017-11-15 11:37:00 -0200

    including validation for cpf

---
+++ src/FluentValidation.Extensions.Br/Validators/CPFValidator.cs
@@ -0,0 +1,23 @@
+﻿namespace FluentValidation.Validators
+{
+    /// <summary>
+    /// Ensures that the property value is a valid CPF number.
+    /// </summary>
+    public class CPFValidator : GenericPersonValidator
+    {
+        internal CPFValidator(int validLength, string errorMessage) 
+            : base(validLength, errorMessage)
+        { }
+
+        public CPFValidator(string errorMessage)
+            : this(11, errorMessage)
+        { }
+
+        public CPFValidator()
+            : this("O CPF é inválido!")
+        { }
+
+        protected override int[] FirstMultiplierCollection => new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+        protected override int[] SecondMultiplierCollection => new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+    }
+}
---
+++ test/FluentValidation.Extensions.Br.Test/CPFValidatorTests.cs
@@ -0,0 +1,45 @@
+﻿namespace FluentValidation.Extensions.Br.Test
+{
+    using Results;
+    using Xunit;
+    using System.Linq;
+
+    public class CPFValidatorTests
+    {
+        [Fact]
+        public void When_CPF_Is_Valid_Then_The_Validator_Should_Pass()
+        {
+            TestExtensionsValidator validator = new TestExtensionsValidator(x => x.RuleFor(r => r.CPF).IsValidCPF());
+            ValidationResult result = validator.Validate(new Person { CPF = "822.420.106-62" });
+
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void When_CPF_Is_Invalid_Then_Set_Custom_Message_And_Validator_Should_Fail()
+        {
+            const string customMessage = "Custom Message";
+
+            TestExtensionsValidator validator = new TestExtensionsValidator(x => x.RuleFor(r => r.CPF).IsValidCPF().WithMessage(customMessage));
+            ValidationResult result = validator.Validate(new Person { CPF = "000.000.000-00" });
+
+            string errorMessage = result.Errors.FirstOrDefault()?.ErrorMessage ?? string.Empty;
+
+            Assert.False(result.IsValid);
+            Assert.Equal(customMessage, errorMessage);
+        }
+
+
+        [Theory]
+        [InlineData("144.442.344-57")]
+        [InlineData("543.434.321-76")]
+        public void When_CPF_Is_Invalid_Then_The_Validator_Should_Fail(string cpf)
+        {
+            TestExtensionsValidator validator = new TestExtensionsValidator(x => x.RuleFor(r => r.CPF).IsValidCPF());
+            ValidationResult result = validator.Validate(new Person { CPF = cpf });
+
+            Assert.False(result.IsValid);
+        }
+
+    }
+}
